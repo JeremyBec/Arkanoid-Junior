5e5ba47335997291d13aefbcba49782e6d705d9e
JeremyBec <jeremy.bec.pro@gmail.com>
2015-03-09 14:16:15 +0100

    Ajout physiques basiques sur la balle et le paddl

--- Arkanoid Junior/GameScene.m
+++ Arkanoid Junior/GameScene.m
@@ -18,7 +18,7 @@
 @implementation GameScene
 
 -(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here */
+    /* Setup de notre scène */
     [self addPaddle];
     [self addBricks];
     [self addBall];
@@ -43,31 +43,51 @@
 }
 
 -(void) addPaddle {
+    // Création du paddle
     _spritePaddle = [SKSpriteNode spriteNodeWithImageNamed:@"paddle"];
     
-    CGPoint initialPosition = CGPointMake(74, 62);
+    CGPoint initialPosition = CGPointMake(self.size.width/2, 62);
     _spritePaddle.position = initialPosition;
-    
+    _spritePaddle.name = @"paddle";
     [self addChild:_spritePaddle];
+    
+    _spritePaddle.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_spritePaddle.frame.size];
+    _spritePaddle.physicsBody.restitution = 0.1f;
+    _spritePaddle.physicsBody.friction = 0.4f;
+    // Rendre le physicsBody static
+    _spritePaddle.physicsBody.dynamic = NO;
 }
 
 -(void) addBricks {
+    // Création des briques
     for (int count2 = 0; count2 < 3; count2++)
     {
         for (int count = 0; count<=3; count++) {
             SKSpriteNode *brickRed = [SKSpriteNode spriteNodeWithImageNamed:@"brick-red"];
             CGPoint brickInitPoint = CGPointMake((brickRed.size.width/2)+ 30 + ((brickRed.size.width + 10) *count), 400 + ((brickRed.size.height + 30) * count2));
             brickRed.position = brickInitPoint;
+            brickRed.name = [NSString stringWithFormat:@"brick%i", count];
             [self addChild:brickRed];
         }
     }
 }
 
 -(void) addBall {
+    // Création de la balle
     SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
     CGPoint ballStartingPoint = CGPointMake(self.size.width/2, 200);
     ball.position = ballStartingPoint;
     [self addChild:ball];
+    
+    // Ajout d'un body "physique" à notre balle
+    ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width/2];
+    // Desactiver la friction sur notre balle
+    ball.physicsBody.friction = 0.0f;
+    // Définir la restitution. La balle rebondira avec autant de force que l'impact
+    ball.physicsBody.restitution = 1.0f;
+    // Simule une friction de l'air, notre balle ne veut pas être ralentie lorsqu'elle bouge.
+    ball.physicsBody.linearDamping = 0.0f;
+    ball.physicsBody.allowsRotation = NO;
 }
 
 @end
