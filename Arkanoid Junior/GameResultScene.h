7815970ec13509b9f29d2178aa17bcf009f54f9e
JeremyBec <jeremy.bec.pro@gmail.com>
2015-03-17 17:13:35 +0100

    Ajout ResultScene et modif bricks

---
+++ Arkanoid Junior/GameResultScene.h
@@ -0,0 +1,13 @@
+//
+//  GameResultScene.h
+//  Arkanoid Junior
+//
+//  Created by Jeremy Bec on 17/03/2015.
+//  Copyright (c) 2015 JeremyBec. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameResultScene : NSObject
+
+@end
---
+++ Arkanoid Junior/GameResultScene.m
@@ -0,0 +1,13 @@
+//
+//  GameResultScene.m
+//  Arkanoid Junior
+//
+//  Created by Jeremy Bec on 17/03/2015.
+//  Copyright (c) 2015 JeremyBec. All rights reserved.
+//
+
+#import "GameResultScene.h"
+
+@implementation GameResultScene
+
+@end
